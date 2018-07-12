d397d45ff73965df86c72ce07a8a469958e66084
Γεώργιος Σαββόπουλος <gsavopul@gmail.com>
2018-07-12 10:59:05 +0300

    Αφαίρεση αρχείων cpp

--- 03_FrogJmp.cpp
+++
@@ -1,9 +0,0 @@
-int solution(int X, int Y, int D)
-{
-	int jumps = (Y-X)/D;
-
-	if ((Y-X)%D != 0)
-		jumps++;
-
-	return jumps;
-}
--- 06_Distinct.cpp
+++
@@ -1,9 +0,0 @@
-# include <algorithm>
-
-int solution(vector<int> &A)
-{
-	sort(A.begin(),A.end());
-	auto x=unique(A.begin(),A.end());
-
-	return x-A.begin();
-}
